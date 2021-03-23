325c7153edc90719aef8a578ea0f961e95e0044c
Marko Prodanovic <marko.prodanovic@outlook.com>
2021-03-23 12:01:44 -0700

    Added progress indicator for courses that match subaccount & term id [x/total_count]

--- src/syllabus_downloader.py
+++ src/syllabus_downloader.py
@@ -28,60 +28,93 @@ import re
 import csv
 import urllib.request
 
+ENROLLMENT_TERMS = {}
+
 # for printing neatly formatted objects (used for debugging)
 pp = pprint.PrettyPrinter(indent=4)
 
+
 def get_course_info(c, canvas, auth_header):
-        try:
-            this_course = {'name': c.name,
-                'id': c.id,
-                'course_code': c.course_code,
-                'term': c.term['name'],
-                'workflow_state': c.workflow_state,
-                'syllabus_body': c.syllabus_body,
-                'course_name_formatted': format_course_code(c.course_code),
-                'extract_details': ''}
-            course_name_formatted = this_course['course_name_formatted']
-            msgs = get_syllabus_info(c, canvas, auth_header, this_course['term'], course_name_formatted)
-            this_course['extract_details'] = msgs
-            print(msgs)
-            return(this_course)
-            
-        except Exception as e:
-            cprint(f'{e}: Error getting data for {c.name}', 'red')
+    try:
+        this_course = {
+            "name": c.name,
+            "id": c.id,
+            "course_code": c.course_code,
+            "term": c.term["name"],
+            "workflow_state": c.workflow_state,
+            "syllabus_body": c.syllabus_body,
+            "course_name_formatted": format_course_code(c.course_code),
+            "extract_details": "",
+        }
+        course_name_formatted = this_course["course_name_formatted"]
+        msgs = get_syllabus_info(
+            c, canvas, auth_header, this_course["term"], course_name_formatted
+        )
+        this_course["extract_details"] = msgs
+        print(msgs)
+        return this_course
+
+    except Exception as e:
+        cprint(f"{e}: Error getting data for {c.name}", "red")
+
+
+def filter_to_term(courses, term):
+    """
+    args:
+        courses (Pagniated list type course): Courses to filter through
+        term (string): ex. "2017W2"
+    """
+
+    count = 0
+    term_courses = []
+
+    for c in courses:
+        if c.term["name"].startswith(term):
+            term_courses.append(c)
+            count += 1
+
+    return term_courses, count
 
 
 def main():
     # get user inputs
     canvas, account, auth_header, term = get_user_inputs()
 
+    cprint(f"\nQuerying courses in {account.name}...\n", "yellow")
+
     # get list of courses
     courses = account.get_courses(include=["term", "syllabus_body"])
-    #courses = courses[0:10]
     course_data = []
 
-   # total = courses.totalCount()
-    
-    for count, c in enumerate(courses, 1):
+    # term_courses is a list, not PaginatedList
+    # course count is the number of courses that match that term id
+    term_courses, total_course_count = filter_to_term(courses, term)
 
-        try: 
-            #TODO seperate the count from the function
-            #TODO add overall counter i.e .1/X
-            if c.term['name'].startswith(term):
-                cprint(f"{count}: Getting data for {c.name}", 'green')
+    # COUNT COURSES BY TRAVERSING THE PAGINATED LIST
+    # total = courses.totalCount()
+
+    for count, c in enumerate(term_courses, 1):
+
+        try:
+            if c.term["name"].startswith(term):
+                cprint(
+                    f"\n{count}: Getting data for {c.name} [{count}/{total_course_count}]",
+                    "green",
+                )
                 try:
                     this_course = get_course_info(c, canvas, auth_header)
                     course_data.append(this_course)
                 except Exception as e:
-                    cprint(f'{count}: Error getting course info - {c.name}', 'red')
-        
+                    cprint(f"{count}: Error getting course info - {c.name}", "red")
+
         except Exception as e:
-            cprint(f'Unresolved error\n:{e} ...\n', 'red')
+            cprint(f"Unresolved error\n:{e} ...\n", "red")
     try:
         df = pd.DataFrame(course_data)
         df.to_csv(f"{term}-syllabus_download-tracking.csv")
     except Exception as e:
-            cprint(f'Unknown error in csv creation\n{e}\n...')
+        cprint(f"Unknown error in csv creation\n{e}\n...")
+
 
 if __name__ == "__main__":
     # execute only if run as a script
